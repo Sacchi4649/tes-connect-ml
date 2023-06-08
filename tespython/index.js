fa1829137f043e2e5233ac16720ee299a8627d0c
Sacchi4649 <sanyadika@gmail.com>
2023-06-08 12:16:34 +0700

    edited index.js

--- tespython/index.js
+++ tespython/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const { spawn } = require("child_process");
-const inputModel = require("./inputSchema");
 const app = express();
-const port = 3000;
-
-app.get("/result", (req, res) => {
-  const input = new inputModel(req.body);
+const port = 8080;
+app.use(express.urlencoded({ extended: true })); //buat parsing url encode
+app.use(express.json());
+app.post("/result", (req, res) => {
   var largeDataSet = [];
-
+  const { keywords, city } = req.body;
+  console.log(req.body);
   // spawn new child process to call the python script
-  console.log(input);
-  const python = spawn("python", ["nawaf.py", "tempat baca buku", "Jakarta"]);
+
+  const python = spawn("python", ["./python/nawaf.py", keywords, city]);
   // collect data from script
   python.stdout.on("data", function (data) {
     console.log("Pipe data from python script ...");
@@ -21,6 +21,7 @@ app.get("/result", (req, res) => {
     console.log(`child process close all stdio with code ${code}`);
     // send data to browser
     res.send(largeDataSet.join(""));
+    // res.json(JSON.parse(largeDataSet));
   });
 });
 app.listen(port, () =>
